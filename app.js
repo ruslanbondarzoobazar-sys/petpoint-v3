650597fbca8655644d881e6792e459e9ecf2b2a9
ruslanbondarzoobazar-sys <ruslan.bondar.zoobazar@gmail.com>
2026-09-22 12:04:41 +0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('PetPoint v3 started');
+const firebaseConfig = {
+  apiKey: "AIzaSyDEM6yhFUy9gyO71-tyg6Guq_q6ixASZIw",
+  authDomain: "try1-11531.firebaseapp.com",
+  databaseURL: "https://try1-11531-default-rtdb.firebaseio.com",
+  projectId: "try1-11531",
+  storageBucket: "try1-11531.firebasestorage.app",
+  messagingSenderId: "677256692016",
+  appId: "1:677256692016:web:a1c11a500cc3b8b090c8f5",
+  measurementId: "G-MXL1QYBJ4K"
+};
