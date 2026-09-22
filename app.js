ee5c919f325b9152a66247cf4b9e3a14b718ecc1
ruslanbondarzoobazar-sys <ruslan.bondar.zoobazar@gmail.com>
2026-09-22 12:30:52 +0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('PetPoint v3 started');
+
 const firebaseConfig = {
   apiKey: "AIzaSyDEM6yhFUy9gyO71-tyg6Guq_q6ixASZIw",
   authDomain: "try1-11531.firebaseapp.com",
@@ -9,3 +10,11 @@ const firebaseConfig = {
   appId: "1:677256692016:web:a1c11a500cc3b8b090c8f5",
   measurementId: "G-MXL1QYBJ4K"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+console.log("Проверка Firebase...");
+
+firebase.database().ref("/test").set("hello")
+  .then(() => console.log("Firebase работает!"))
+  .catch(err => console.error("Firebase ошибка:", err));
